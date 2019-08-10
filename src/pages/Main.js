b1819ee883f54bedc137f1c5e71f720840a0d634
iotidev <iotidevelopment@gmail.com>
2019-08-10 02:33:14 -0300

    Implementação Real Time

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,14 +3,18 @@ import { Link } from 'react-router-dom';
 
 import './Main.css';
 
+import io from 'socket.io-client';
+
 import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ match }) {
   const [ devs, setDevs ] = useState([]);
+  const [ matchDev, setMatchDev ] = useState(null);
 
   useEffect(() => {
     async function loadDevs() {
@@ -24,6 +28,16 @@ export default function Main({ match }) {
     }
 
     loadDevs();
+  }, [ match.params.id ]);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3333', {
+      query: { user: match.params.id }
+    });
+
+    socket.on('match', dev => {
+      setMatchDev( dev );
+    });
   }, [ match.params.id ]);
 
   async function handleLike( id ){
@@ -77,6 +91,19 @@ export default function Main({ match }) {
         </ul>
       ) : (
           <div className="empty">Acabou! :(</div>
+      )}
+
+      { matchDev && (
+        <div className="match-container" >
+          <img src={ itsamatch } alt="It's a match" />
+
+          <img className="avatar" src={ matchDev.avatar } alt={ matchDev.name } />
+
+          <strong>{ matchDev.name }</strong>
+          <p>{ matchDev.bio }</p>
+
+          <button type="button" onClick={ () => setMatchDev( null ) }>FECHAR</button>
+        </div>
       )}
     </div>
   );
